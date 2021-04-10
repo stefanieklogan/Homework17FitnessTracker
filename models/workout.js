75be8f4ae4ab6456d290d34719a35d194178209a
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-04-10 19:14:33 -0400

    workout.js

---
+++ models/workout.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//individual workout
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now()
+        },
+        exercises: {
+            type: Array,
+            default: []
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+//Total duration calc
+
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout
