88d46ddcf914f83882cf1174ece3c1b6b8d02f0c
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-04-10 19:07:39 -0400

    fix morgan requirement in sever

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const morgan = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000
