55a5f94788314dff938764a5aaafdc57b6ddfa59
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-04-11 17:03:25 -0400

    api routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,9 +1,33 @@
 var db = require("..models/workout");
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
 
 //get all workouts
+router.get("/api/workouts", function (req, res) {
+    db.find({}).then(function (workout) {
+        res.send(workout);
+    })
+        .catch(function (err) {
+            res.send(err);
+        });
+});
 
 //create workout
+router.post("/api/workouts", function (req, res) {
+    db.create(req.body).then(function (data) {
+        res.send(data)
+    })
+})
 
 //update workout by id
+router.put("/api/workouts/:id", function (req, res) {
+    var id = req.params.id;
+    db.findByAndUpdate(id, { $push: { exercises: req.body } })
+        .then(function (data) {
+            res.send(data)
+        });
+});
 
 //get stats page
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000
 
@@ -12,15 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-require("./routes/htmlroutes.js");
-require("./routes/apiroutes.js");
+
+app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
-// app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
