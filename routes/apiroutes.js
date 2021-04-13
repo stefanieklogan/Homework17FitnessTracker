4653bb387e22b15dd35377dd967b58e0dcd1dfa7
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-04-12 22:07:42 -0400

    formatted routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,46 +2,43 @@ var db = require("../models/workout");
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-module.exports = function(app) {
-//get all workouts
-app.get("/api/workouts", function (req, res) {
-    db.find({}).then(function (workout) {
-        res.send(workout);
-    })
-        .catch(function (err) {
-            res.send(err);
-        });
-});
-
-//create workout
-app.post("/api/workouts", function (req, res) {
-    db.create(req.body).then(function (data) {
-        res.send(data)
-    })
-    .catch(function (err) {
-        res.send(err);
+module.exports = function (app) {
+    //get all workouts
+    app.get("/api/workouts", function (req, res) {
+        db.find({}).then(function (workout) {
+            res.send(workout);
+        })
+            .catch(function (err) {
+                res.send(err);
+            });
     });
-})
 
-//update workout by id
-app.put("/api/workouts/:id", function(req,res){
-    var id = req.params.id
-    db.findByIdAndUpdate(id, {$push: {exercises: req.body}})
-    .then(function(data){
-        res.send(data)
-    })
-});
-
-//get stats page
-app.get("/api/workouts/range", (req,res) => {
-    db.find({})
-    .then(workout => {
-      res.json(workout);
-    })
-    .catch(err => {
-      res.json(err);
+    //create workout
+    app.post("/api/workouts", function (req, res) {
+        db.create(req.body).then(function (data) {
+            res.send(data)
+        })
     });
-  });
 
-  //final bracket for module export
+    //update workout by id
+    app.put("/api/workouts/:id", function (req, res) {
+        var id = req.params.id
+        db.findByIdAndUpdate(id, { $push: { exercises: req.body } })
+            .then(function (data) {
+                res.send(data)
+            })
+    });
+
+    //get stats page
+    app.get("/api/workouts/range", (req, res) => {
+        db.find({})
+            .then(workout => {
+                res.json(workout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    //final bracket for module export
 }
